d767de8f4444fcd0df99d81d457f37e68a7ee59b
Eric Escalante <eric.m.escalante@gmail.com>
2019-03-11 08:55:59 -0500

    Finished Working with Files

--- 4.6_working_with_files.py
+++ 4.6_working_with_files.py
@@ -1,4 +1,4 @@
-# Read the contents of your last exercise file into a variable.
+`# Read the contents of your last exercise file into a variable.
 # Print out every line in the file
 
 # with open('4.5_import_exercises.py') as f:
@@ -29,4 +29,6 @@ with open('my_grocery_list.txt', 'r') as rf:
 
 # Create a function named buy_item. It should accept the name of an item on the grocery list, and remove that item from the list.
 
-
+def buy_item():
+    with open('my_grocery_list.txt', 'a+') as f:
+        f.write(test)
