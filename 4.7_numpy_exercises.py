3b824abcdb3d9c0ad7454fe05b9414ab7b115ded
Eric Escalante <eric.m.escalante@gmail.com>
2019-03-12 15:09:39 -0500

    cont numpy exercises

--- 4.7_numpy_exercises.py
+++ 4.7_numpy_exercises.py
@@ -42,7 +42,7 @@ aa = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 # Exercise 1 - Make a variable called sum_of_aa to hold the sum of all the numbers in above list
 
 sum_of_aa = aa.sum()
-print('Sum of al the numbers: {}'.format(sum_of_aa))
+print('Sum of all the numbers: {}'.format(sum_of_aa))
 
 # Exercise 2 - Make a variable named min_of_aa to hold the minimum of all the numbers in the above list
 min_of_aa = aa.min()
@@ -72,4 +72,55 @@ print('Odd Numbers: {}'.format(odds_in_aa))
 evens_in_aa = (aa[aa %2==0])
 print('Even Numbers: {}'.format(evens_in_aa))
 print('\n')
+
+## What about life in two dimensions? A list of lists is matrix, a table, a spreadsheet, a chessboard...
+## Setup 2: Consider what it would take to find the sum, min, max, average, sum, product, and list of squares for this list of two lists.
+b = np.array([
+    [3, 4, 5],
+    [6, 7, 8]
+])
+
+# Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable. **Hint, you'll first need to make sure that the "b" variable is a numpy array**
+sum_of_b = b.sum()
+print('Sum of b: {}'.format(sum_of_b))
+
+# Exercise 2 - refactor the following to use numpy.
+min_of_b = b.min()
+print('Min of b: {}'.format(min_of_b))
+
+# Exercise 3 - refactor the following maximum calculation to find the answer with numpy.
+max_of_b = b.max()
+print('Max of b: {}'.format(max_of_b))
+
+# Exercise 4 - refactor the following using numpy to find the mean of b
+mean_of_b = b.mean()
+print('Mean of b: {}'.format(mean_of_b))
+
+# Exercise 5 - refactor the following to use numpy for calculating the product of all numbers multiplied together.
+product_of_b = b.prod()
+print('Product of b: {}'.format(product_of_b))
+
+# Exercise 6 - refactor the following to use numpy to find the list of squares 
+squares_of_b = ([i**2 for i in b])
+print('Squares of b: {}'.format(squares_of_b))
+
+# Exercise 7 - refactor using numpy to determine the odds_in_b
+odds_in_b = (aa[aa %2==1])
+print('Odds in b: {}'.format(odds_in_b))
+
+# Exercise 8 - refactor the following to use numpy to filter only the even numbers
+evens_in_b = (b[b %2==0])
+print('Even Numbers: {}'.format(evens_in_b))
+
+# Exercise 9 - print out the shape of the array b.
+print('The shape of array b: {}'.format(b.shape))
+
+# Exercise 10 - transpose the array b.
+print('The view of array with axes transposed: {}'.format(b.transpose()))
+
+# Exercise 11 - reshape the array b to be a single list of 6 numbers. (1 x 6)
+print('Reshaped b to be a single list of 6: {}'.format(b.reshape(1,6)))
+
+# Exercise 12 - reshape the array b to be a list of 6 lists, each containing only 1 number (6 x 1)
+print('Reshaped b to be a single list of 6: {}'.format(b.reshape(6,1)))
 
