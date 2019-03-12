1c1cccad642fbac4d1c269185d139b44f32e130a
Eric Escalante <eric.m.escalante@gmail.com>
2019-03-12 14:46:23 -0500

    cont numpy exercises

--- 4.7_numpy_exercises.py
+++ 4.7_numpy_exercises.py
@@ -32,3 +32,44 @@ print('Centered Data: {}'.format(a - a.mean()))
 
 print('Z-score for each data point: {}'.format((a - a.mean() / a.std())))
 print('\n')
+
+# 8. Copy the setup and exercise directions from More Numpy Practice into your 4.7_numpy_exercises.py and add your solutions.
+
+## Setup 1
+aa = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# Use python's built in functionality/operators to determine the following:
+# Exercise 1 - Make a variable called sum_of_aa to hold the sum of all the numbers in above list
+
+sum_of_aa = aa.sum()
+print('Sum of al the numbers: {}'.format(sum_of_aa))
+
+# Exercise 2 - Make a variable named min_of_aa to hold the minimum of all the numbers in the above list
+min_of_aa = aa.min()
+print('Minimum of all numbers: {}'.format(min_of_aa))
+
+# Exercise 3 - Make a variable named max_of_aa to hold the max number of all the numbers in the above list
+max_of_aa = aa.max()
+print('Maximum of all numbers: {}'.format(max_of_aa))
+
+# Exercise 4 - Make a variable named mean_of_aa to hold the average of all the numbers in the above list
+mean_of_aa = aa.mean()
+print('Mean of all numbers: {}'.format(mean_of_aa))
+
+# Exercise 5 - Make a variable named product_of_aa to hold the product of multiplying all the numbers in the above list together
+product_of_aa = aa.prod()
+print('Product of all numbers: {}'.format(product_of_aa))
+
+# Exercise 6 - Make a variable named squares_of_aa. It should hold each number in a squared like [1, 4, 9, 16, 25...]
+squares_of_aa = [i**2 for i in aa]
+print('Each number squared: {}'.format(squares_of_aa))
+
+# Exercise 7 - Make a variable named odds_in_aa. It should hold only the odd numbers
+odds_in_aa = (aa[aa %2==1])
+print('Odd Numbers: {}'.format(odds_in_aa))
+
+# Exercise 8 - Make a variable named evens_in_aa. It should hold only the evens.
+evens_in_aa = (aa[aa %2==0])
+print('Even Numbers: {}'.format(evens_in_aa))
+print('\n')
+
